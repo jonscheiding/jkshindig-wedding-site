4d8ea3c2471998c10593e400c6f4414a511dab6b
Jon Scheiding <jonscheiding@gmail.com>
2018-08-23 17:28:25 -0400

    Starting to componentize a bit

--- src/App.js
+++ src/App.js
@@ -1,109 +1,18 @@
 import React, { Component } from 'react';
-
 import styled from 'styled-components';
-import { Parallax, Background } from 'react-parallax';
 
-const BackgroundImage = styled.div`
-  background-image: url('/splash-image.jpg');
-  background-size: cover;
-  background-position: center;
+import Splash from './components/Splash';
 
-  height: 100vh;
-  width: 100vw;
-
-  filter: blur(3px);
-  transform: scale(1.05);
-`;
-
-const BackgroundOverlay = styled.div`
-  height: 100vh;
-  width: 100vw;
-
-  position: absolute;
-  top: 0;
-  z-index: -1;
-
-  background: linear-gradient(
-    rgba(33, 48, 61, 1),
-    rgba(33, 48, 61, 0.5) 20%,
-    rgba(0, 0, 0, 0) 40%
-  );
-  /* /background-color: rgba(33, 48, 61, 0.5); */
-`
-
-const Header1 = styled.h1`
-  text-align: center;
-  text-transform: uppercase;
-
-  font-size: 7vh;
-  padding: 2vh;
-  margin: 0;
-`;
-
-const Header2 = styled.h2`
-  text-align: center;
-  text-transform: uppercase;
-
-  font-size: 5vh;
-  padding: 2vh;
-  margin: 0;
-`
-
-const Script = styled.span`
-  text-transform: none;
-  font-family: 'Allura', 'Zeyada', 'La Belle Aurore', cursive;
-`;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Parallax strength={350} style={{height: '100vh'}}>
-          <Background>
-            <BackgroundImage />
-          </Background>
-          <div style={{height: '100vh'}}>
-            <Header1>
-              Kaleigh <Script>and</Script> Jonathan
-            </Header1>
-            <Header2>March 29, 2019</Header2>
-            <Header2 style={{position: 'absolute', bottom: 0, width: '100%', boxSizing: 'border-box'}}>
-              The Newport Syndicate
-              <Script style={{display: 'block', fontSize: '3vh'}}>
-                <div>18 East 5th Street</div>
-                <div>Newport, Kentucky 41073</div>
-              </Script>
-            </Header2>
-          </div>
-        </Parallax>
-        <div style={{position: 'sticky', top: 0}}>
-          <ul>
-            <li>OUR STORY</li>
-            <li>EVENT</li>
-            <li>ATTENDANTS</li>
-            <li>GIFTS</li>
-            <li>RSVP</li>
-          </ul>
+        <Splash />
+        <div style={{height: '1000vh'}}>
+          {/* Placeholder to test out parallax effects */}
         </div>
-        <div style={{height: '1000vh'}} />
       </div>
-      // <div>
-      //   <BackgroundImage />
-      //   <BackgroundOverlay />
-      //   <Header1>
-      //     Kaleigh <Script>and</Script> Jonathan
-      //   </Header1>
-      //   <Header2>
-      //     March 29, 2019
-      //   </Header2>
-      //   <Header2 style={{position: 'absolute', bottom: '1vh', width: '100%', boxSizing: 'border-box'}}>
-      //     Newport Syndicate
-      //     <Script style={{display: 'block', fontSize: '3vh'}}>
-      //       <div>18 East 5th Street</div>
-      //       <div>Newport, Kentucky 41073</div>
-      //     </Script>
-      //   </Header2>
-      // </div>
     );
   }
 }
---
+++ src/components/Splash.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Parallax, Background } from 'react-parallax';
+import styled from 'styled-components';
+
+import image from '../assets/splash-image.jpg';
+
+export default class Splash extends Component {
+  render() {
+    const BackgroundImage = styled.div`
+      background-image: url(${image});
+      background-size: cover;
+      background-position: center;
+    
+      height: 100vh;
+      width: 100vw;
+    
+      filter: blur(3px);
+      transform: scale(1.05);
+    `;
+
+    const Overlay = styled.div`
+      height: 100%;
+      width: 100%;
+
+      position: absolute;
+      top: 0;
+    `;
+
+    const SplashOverlay = styled(Overlay)`
+      background: linear-gradient(
+        rgba(33, 48, 61, 1),
+        rgba(33, 48, 61, 0.25) 25%,
+        rgba(0, 0, 0, 0) 50%
+      );
+    `;
+
+    const Bottom = styled.div`
+      position: absolute;
+      bottom: 1vh;
+      width: 100%;
+    `;
+
+    return (
+      <Parallax strength={350}>
+        <Background>
+          <BackgroundImage />
+          <SplashOverlay />
+        </Background>
+        <div style={{height: '100vh'}}>
+          <div style={{zIndex: '1'}}>
+            <h1>Kaleigh <span className='script'>and</span> Jonathan</h1>
+            <h2>March 29, 2019</h2>
+            <h2 className='script'>save the date</h2>
+            <Bottom>
+              <h2>Newport Syndicate</h2>
+              <h3 className='script'>
+                <div>18 East 5th Street</div>
+                <div>Newport, Kentucky 41073</div>
+              </h3>
+            </Bottom>
+          </div>
+        </div>
+      </Parallax>
+    );
+  }
+}
