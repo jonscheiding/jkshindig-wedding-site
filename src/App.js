343c7fb0d1f8e4c7c3f7c26cc3d08a7e5e95918b
Jon Scheiding <jonscheiding@gmail.com>
2018-08-23 17:35:44 -0400

    ESLint settings change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
 import Splash from './components/Splash';
-
 
 class App extends Component {
   render() {
     return (
       <div>
         <Splash />
-        <div style={{height: '1000vh'}}>
-          {/* Placeholder to test out parallax effects */}
-        </div>
       </div>
     );
   }
