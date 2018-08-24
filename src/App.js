22950bc61e653314e3f1c6a500a80342000f7961
Jon Scheiding <jonscheiding@gmail.com>
2018-08-24 11:44:11 -0400

    Made title dynamic from content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import flourishImage from './assets/large-flourish.png';
 import Splash from './components/Splash';
 import NavigationMenu from './components/NavigationMenu';
 import sections from './data/sections';
+import content from './data/content';
 
 const LargeFlourish = styled.div`
   background-image: url(${flourishImage});
@@ -26,6 +27,11 @@ class App extends Component {
         {sections.map(s => s.component)}
       </div>
     );
+  }
+
+  componentDidMount() {
+    const names = content.spouses.map(s => s.name.first);
+    document.title = `${names[0]} and ${names[1]}`;
   }
 }
 
