b9970e276d06e314513c9ef2f43fbd422b94b3d3
Jon Scheiding <jonscheiding@gmail.com>
2018-08-24 10:06:23 -0400

    ESLint fixes

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <App />,
   rootEl
-)
+);
 
 if (module.hot) {
   module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
+    const NextApp = require('./App').default;
     ReactDOM.render(
       <NextApp />,
       rootEl
-    )
-  })
+    );
+  });
 }
