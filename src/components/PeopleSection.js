0268d988177fdacec5ca2d62ee6f35326bba2f92
Jon Scheiding <jonscheiding@gmail.com>
2018-08-24 20:37:55 -0400

    Start on People section

---
+++ src/components/PeopleSection.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class PeopleSection extends Component {
+  render() {
+    return (<div>People</div>);
+  }
+}
--- src/data/sections.js
+++ src/data/sections.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
-/* PLACEHOLDER STUFF */
-const StyledSection = styled.section`
-  padding-top: 2.5rem;
-  text-align: center;
-  font-size: 3rem;
-  height: 70vh;
-  text-transform: uppercase;
-`;
+import PeopleSection from '../components/PeopleSection';
 
-const NamedSection = ({name}) => (<StyledSection id={name}>{name}</StyledSection>);
-NamedSection.propTypes = {
-  name: PropTypes.string.isRequired
-};
+const wrap = (element, id) => (<div id={id}>{element}</div>);
 
-const ids = [ 'people', 'events', 'gifts', 'rsvp' ];
-export default ids.map(id => ({
-  id, component: <NamedSection name={id} />
-}));
+export default [
+  { id: 'people', component: wrap(<PeopleSection />, 'people') }
+];
