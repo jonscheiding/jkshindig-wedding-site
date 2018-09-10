9fd1b7df33724f9344462d0f812ca7c67323e118
Jon Scheiding <jonscheiding@gmail.com>
2018-09-09 23:15:04 -0400

    Made hack-height mode environment-configurable

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ class App extends Component {
   render() {
     const { location, date, spouses } = content;
 
+    const style = { height: process.env.REACT_APP_HACK_HEIGHT ? '10000px' : 'auto' };
+
     return (
-      <Splash 
-        location={location} date={date} 
-        names={spouses.map(s => s.name.first)} />
+      <div style={style}>
+        <Splash 
+          location={location} date={date} 
+          names={spouses.map(s => s.name.first)} />
+      </div>
     );
   }
 }
