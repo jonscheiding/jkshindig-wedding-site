2b26570a43300a91be90ca78c095b55e3cc9f61f
Jon Scheiding <jonscheiding@gmail.com>
2018-09-10 22:41:28 -0400

    Added attendants content and alternating headshot display

--- src/PropTypesCustom.js
+++ src/PropTypesCustom.js
@@ -21,22 +21,29 @@ const location = PropTypes.exact({
   address: address.isRequired
 });
 
-const spouse = PropTypes.exact({
+const person = PropTypes.exact({
   name: name.isRequired,
-  nickname: PropTypes.string.isRequired,
+  nickname: PropTypes.string,
   title: PropTypes.string.isRequired
 });
 
+const attendant = PropTypes.exact({
+  person: person
+});
+
+const spouse = person;
 const spouses = PropTypes.arrayOf(spouse);
+const attendants = PropTypes.arrayOf(attendant);
 
 const content = PropTypes.exact({
   spouses: spouses.isRequired,
+  attendants: attendants.isRequired,
   story: PropTypes.string.isRequired,
   date: date.isRequired,
   location: location.isRequired
 });
 
 const PropTypesEx = { date, children };
-const PropTypesContent = { date, name, address, location, spouse, content };
+const PropTypesContent = { date, name, address, location, person, spouse, spouses, attendant, attendants, content };
 
 export { PropTypesEx, PropTypesContent };
--- src/components/sections/Stories.js
+++ src/components/sections/Stories.js
@@ -2,13 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-material-responsive-grid';
 
+import { PropTypesContent } from '../../PropTypesCustom';
 import PersonProfile from '../PersonProfile';
 
-const Stories = ({content}) => (
+const Attendant = ({attendant, index}) => (
+  <Row reverse={index % 2 === 0}>
+    <Col xs4={2}>
+      <PersonProfile person={attendant.person} />
+    </Col>
+  </Row>
+);
+
+const Spouses = ({spouses, story}) => (
   <div>
     <Row>
       <Col sm8={1} md={2} />
-      {content.spouses.map((s, i) => (
+      {spouses.map((s, i) => (
         <Col key={i} xs4={2} xs8={4} sm8={3} md={4}>
           <PersonProfile person={s} />
         </Col>
@@ -16,14 +25,33 @@ const Stories = ({content}) => (
     </Row>
     <Row>
       <Col xs4={4} sm8={6} md={8} sm8Offset={1} mdOffset={2}>
-        <p>{content.story}</p>
+        <p>{story}</p>
       </Col>
     </Row>
   </div>
 );
 
+const Stories = ({content}) => (
+  <div>
+    <Spouses spouses={content.spouses} story={content.story} />
+    {content.attendants.map((a, i) => 
+      <Attendant key={i} attendant={a} index={i} />
+    )}
+  </div>
+);
+
+Attendant.propTypes = {
+  attendant: PropTypesContent.attendant,
+  index: PropTypes.number.isRequired
+};
+
+Spouses.propTypes = {
+  spouses: PropTypesContent.spouses.isRequired,
+  story: PropTypes.string.isRequired
+};
+
 Stories.propTypes = {
-  content: PropTypes.object
+  content: PropTypesContent.content
 };
 
 export default Stories;
