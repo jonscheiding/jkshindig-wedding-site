4e4ed999fd5371fe83a6b6e87daa7ca2e179630e
Jon Scheiding <jonscheiding@gmail.com>
2018-09-12 17:05:00 -0400

    Loaders to load JSON from remote server, and load it in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+/* eslint import/no-webpack-loader-syntax: off */
+const content = require('json-loader!remote-loader!env-loader?CONTENT_JSON_URL!');
+content.date = new Date(Date.parse(content.date));
+
 class App extends Component {
   render() {
     return (
