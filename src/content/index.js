82632ac6c3d10eb58451bd6651800bc6877d4f0e
Jon Scheiding <jonscheiding@gmail.com>
2018-09-17 19:39:51 -0400

    Removed static content

--- src/content/index.js
+++
@@ -1,8 +0,0 @@
-export function getImage(name, type) {
-  let filename = name;
-  if(Array.isArray(filename)) {
-    filename = filename.join('.');
-  }
-  filename = filename.replace(/[^a-zA-Z.]*/g, '');
-  return require(`./images/${type ? type + '/' : '' }${filename}.png`);
-}
