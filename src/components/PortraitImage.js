24c9a4a30ebc4d4b81d99a3b34129050cae7166f
Jon Scheiding <jonscheiding@gmail.com>
2018-09-25 12:23:05 -0400

    Updated photo overlay to use SVG instead of image

--- src/components/PortraitImage.js
+++ src/components/PortraitImage.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Parallax } from 'react-parallax';
+import { generate as generateId } from 'shortid';
 
 import Square from './Square';
-import BackgroundImage from './BackgroundImage';
-import overlay from '../assets/picture-overlay.png';
 import styled from 'styled-components';
-
-const OverlaySquare = styled(Square)`
-  transform: scale(1.05);
-  width: 100%;
-`;
+import theme from '../theme.json';
 
 const SizedParallax = styled(Parallax)`
   margin: auto; 
   max-width: ${p => p.half ? '50%' : '100%'};
 `;
 
+const CircleOverlay = () => {
+  const id = generateId();
+
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' width='100%' height='100%'>
+      <defs>
+        <mask id={`mask-${id}`} maskContentUnits='userSpaceOnUse'>
+          <filter id={`filter-${id}`} x='-40%' y='-40%' width='180%' height='180%' filterUnits='userSpaceOnUse'>
+            <feGaussianBlur in='SourceGraphic' stdDeviation='2.5' />
+          </filter>
+          <rect width='100%' height='100%' fill='white' />
+          <circle cx='50%' cy='50%' r='42%' fill='black' style={{filter: `url(#filter-${id})`}} />
+        </mask>
+      </defs>
+      <rect width='100%' height='100%' fill={theme['background-color']} mask={`url(#mask-${id})`} />
+    </svg>
+  );
+};
+
 const PortraitImage = ({image, ...props}) => {
   return (
     <SizedParallax strength={20} bgImage={image} {...props}>
-      <OverlaySquare>
-        <BackgroundImage image={overlay} style={{height: '100%'}} />
-      </OverlaySquare>
+      <Square>
+        <CircleOverlay />
+      </Square>
     </SizedParallax>
   );
 };
